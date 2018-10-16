fb1aa00b7563052696f222955abba59077996fb1
tmpITK <torok.mark.patrik@gmail.com>
2018-10-16 12:36:58 +0200

    Simplified and IF models now work. Problem with simplified was the loading of mod files, that is now handled with neuron.load_mechanisms, which is also called in load_neuron. Problem with IF was that Optimizer could not handle multiple processes, that is now handled by creating intermediate files with name of PID+TIME to be able to uniquely identify them between processes

--- optimizer/modelHandler.py
+++ optimizer/modelHandler.py
@@ -1,16 +1,16 @@
 import os
 import sys
-from string import index,split
 from traceHandler import Trace
 import optimizerHandler
+import importlib
 
 try:
-    import copy_reg
+    import copyreg
 except:
     import copyreg
 
 from types import MethodType
-from neuron import h
+
 
 
 class externalHandler():
@@ -50,7 +50,7 @@ class externalHandler():
         for n in range(self.number_of_params):
             o.SetObjTOOpt("parameter"+str(n))
 
-    def GetExec(self):
+    def GetExec(self ,pid='', creation_time=''):
         """
         Creates the command that runs the simulator with the model and with the appropriate options.
         :return: a ``list`` of strings ready for execution
@@ -58,7 +58,7 @@ class externalHandler():
         tmp=[self.executable,self.model_file]
         for o in self.options:
             tmp.append(o)
-
+        tmp += [pid, creation_time]
         return tmp
 
     def GetParameters(self):
@@ -84,14 +84,24 @@ class modelHandlerNeuron():
     :param base: the base working directory
 
     """
+
+
     def __init__(self,model_path,special_path,base=os.getcwd()):
-        #print "init"
+        from neuron import h
+        import neuron
+
+
+        print('*********** NEURON LOADED ***********')
         self.base_directory=base
         self.special=special_path
         self.model=model_path
-        os.chdir(self.special)
+  
+        neuron.load_mechanisms(self.special)
+        
+        #os.chdir(self.special)
         #from neuron import h
         #from nrn import *
+        
 
         self.hoc_obj=None
         self.vec=None
@@ -102,6 +112,9 @@ class modelHandlerNeuron():
 
     def load_neuron(self):
         #from neuron import h
+        from neuron import h
+        import neuron
+        neuron.load_mechanisms(self.special)
         self.hoc_obj=h
         self.hoc_obj.load_file(str(self.model))
         self.hoc_obj.load_file("stdrun.hoc")
@@ -206,6 +219,7 @@ class modelHandlerNeuron():
             not the actual parameters of the stimulus. This is necessary for Neuron in order to work.
 
         """
+
         self.hoc_obj.load_file("vplay.hoc")
         self.parameters=params
 
@@ -250,7 +264,8 @@ class modelHandlerNeuron():
         sec = self.hoc_obj.cas()
         lseg = [seg for seg in sec]
         #self.hoc_obj.cas().__setattr__(params,values)
-        lseg[int(segment)].__setattr__(params,values)
+        
+        setattr(lseg[int(segment)],params,values)
         self.hoc_obj.pop_section()
 
 
@@ -271,14 +286,16 @@ class modelHandlerNeuron():
             :param values: the value to set
 
         """
+
         try:
             self.sections[section].push()
-            self.hoc_obj.cas().__setattr__(params,values)
+            #self.hoc_obj.cas().__setattr__(params,values)
+            setattr(self.hoc_obj.cas(), params, values)
             self.hoc_obj.pop_section()
         except KeyError:
             sys.exit("No section named " + str(section) + " was found!")
         except AttributeError:
-            self.hoc_obj.cas()(0.5).point_processes()[1].__setattr__(params,values)
+            setattr(self.hoc_obj.cas()(0.5).point_processes()[1], params,values)
 
         #except:
             #sys.exit("Morphology parameter "+params+" not found!")
@@ -305,7 +322,7 @@ class modelHandlerNeuron():
 
         for n in string:
             try:
-                index(n, ss)
+                str.index(n, ss)
                 temp=temp+" "+str(n)
             except ValueError:
                 pass
